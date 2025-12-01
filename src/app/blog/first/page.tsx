8aa1b04f7b274ae1f0d580af0621916634a0fab4
ashraful-techO <islam.ashraful.cse.97@gmail.com>
2025-12-01 16:45:58 +0600

    first commit

---
+++ src/app/blog/first/page.tsx
@@ -0,0 +1,3 @@
+export default function First() {
+  return <h1>My Blog First page</h1>;
+}
---
+++ src/app/products/page.tsx
@@ -0,0 +1,10 @@
+export default function ProductList() {
+  return (
+    <>
+      <h1>Product Lists</h1>
+      <h3>Product1</h3>
+      <h3>Product2</h3>
+      <h3>Product3</h3>
+    </>
+  );
+}
